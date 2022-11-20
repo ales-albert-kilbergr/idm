fb6feb85b219a42d00b9cb00810e9db48bebf754
ales albert KILBERGR <ales.albert@kilbergr.eu>
2022-11-20 23:29:33 +0100

    feat(web-service): add swagger support

--- apps/web-service/src/main.ts
+++ apps/web-service/src/main.ts
@@ -3,13 +3,17 @@
  * This is only a minimal backend to get started.
  */
 
+import { colorizeUrl } from '@idm/node-logger-utils';
 import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerModule
+} from '@nestjs/swagger';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { AppModule } from './app/app.module';
 import { IDMConfigService } from './config';
-import { colorizeUrl } from '@idm/node-logger-utils';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
@@ -20,13 +24,20 @@ async function bootstrap() {
   // differently. The `WINSTON_LOGGER_PROVIDER` configures logger transports,
   // where app logs are stored.
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  // Setup swagger generated API
+  SwaggerModule.setup(
+    'doc',
+    app,
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder().setTitle('IDM web service').build()
+    )
+  );
 
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   await app.listen(config.get('PORT'));
   logger.log(
     `Application is running on: ` +
-      colorizeUrl(`http://localhost:${config.get('PORT')}/${globalPrefix}}`)
+      colorizeUrl(`http://localhost:${config.get('PORT')}`)
   );
 }
 
